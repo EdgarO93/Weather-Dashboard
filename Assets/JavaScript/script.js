eb4caab900e07921c8cabbf2710de742923fe7e8
Edgar Ortega <edort93@gmail.com>
2021-09-02 23:45:24 -0500

    updated api to forescast and imperial units

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -6,7 +6,7 @@ function searchButtonHandler() {
     searchInput = "chicago"
 
     let apiKey = "b21a3a98c66a430705a9406d77dd7091";
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}`
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&units=imperial&appid=${apiKey}`
 $.ajax({
         url: weatherUrl,
         success: function (data) {
